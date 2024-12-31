536a64ed2a0d4ae4da6e8e926d7664ff7ad4b2a0
jradziejewski <192462@student.ue.wroc.pl>
2024-12-31 18:26:38 +0100

    add cleanInput function

--- repl_test.go
+++ repl_test.go
@@ -5,4 +5,39 @@ import (
 )
 
 func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "              bye                universe            whatever",
+			expected: []string{"bye", "universe", "whatever"},
+		},
+		{
+			input:    "   hello",
+			expected: []string{"hello"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Fatalf("for input %q, expected length %d but got %d", c.input, len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("want %s, got %s", expectedWord, word)
+			}
+		}
+	}
 }
